b24e93ea7f419131ab5c2ccbf5f7b4fc000c076e
Sebastian Bommer <sebastan.bommer.sbg@gmail.com>
2024-05-28 12:37:41 -0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -11,31 +11,11 @@ function App() {
     <>
       <h1>Sebs test app! Updated git repo.</h1>
 
-      <FormID
+      {/* <FormID
         disp_string={["Form 1", "Form 2", "Form 3"]}
         disp_heading={"Heading"}
         country_choises={["Austra"]}
-      />
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      /> */}
     </>
   );
 }
