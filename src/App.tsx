9411b9a988a059e5ba937de21fc753f2c2fbcbb8
Sebastian Bommer <sebastan.bommer.sbg@gmail.com>
2024-05-28 12:52:31 -0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
-// import { FormID } from "./components/FormID";
+import { FormID } from "./components/FormID";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,11 +11,11 @@ function App() {
     <>
       <h1>Sebs test app! Updated git repo.</h1>
 
-      {/* <FormID
+      <FormID
         disp_string={["Form 1", "Form 2", "Form 3"]}
         disp_heading={"Heading"}
         country_choises={["Austra"]}
-      /> */}
+      />
     </>
   );
 }
